3a12a442807c435e5cc48208636450ec8d3b5298
Tom Guimberteau <tomguimberteau@gmail.com>
2016-07-27 16:21:54 -0400

    Update loan-editor.php

--- fpdf181/loan-editor.php
+++ fpdf181/loan-editor.php
@@ -14,7 +14,7 @@ $fields = array(
     'phone'   => 'My phone number'
 );
 
-$pdf = new FPDM('template.pdf');
+$pdf = new FPDM('LoanAgreement.pdf');
 $pdf->Load($fields, false); // second parameter: false if field values are in ISO-8859-1, true if UTF-8
 $pdf->Merge();
 $pdf->Output();
