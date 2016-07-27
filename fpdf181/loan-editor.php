6b04ff1267f83d828a580717fd6c38d396c54c3c
Tom Guimberteau <tomguimberteau@gmail.com>
2016-07-27 16:15:04 -0400

    Update loan-editor.php

--- fpdf181/loan-editor.php
+++ fpdf181/loan-editor.php
@@ -1,11 +1,21 @@
 #!/usr/bin/php
 <?php
 
-require('fpdf.php');
-$pdf=new FPDF();
-$pdf->AddPage();
-$pdf->SetFont('Arial','',11);
-$pdf->Cell(0,10,"Loan Agreement",1,1,"C");
-$pdf->output();
+/***************************
+  Sample using a PHP array
+****************************/
 
+require('fpdm.php');
+
+$fields = array(
+    'name'    => 'My name',
+    'address' => 'My address',
+    'city'    => 'My city',
+    'phone'   => 'My phone number'
+);
+
+$pdf = new FPDM('template.pdf');
+$pdf->Load($fields, false); // second parameter: false if field values are in ISO-8859-1, true if UTF-8
+$pdf->Merge();
+$pdf->Output();
 ?>
