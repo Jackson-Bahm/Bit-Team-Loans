e9f8853a0fea22028238aa56d8807c49a3eb662f
Tom Guimberteau <tomguimberteau@gmail.com>
2016-07-28 23:06:22 -0400

    Update loan-editor.php

--- fpdf181/loan-editor.php
+++ fpdf181/loan-editor.php
@@ -1,21 +1,35 @@
 #!/usr/bin/php
-<?php
+<?php 
+require('fpdf.php'); 
 
-/***************************
-  Sample using a PHP array
-****************************/
+//create a FPDF object
+$pdf=new FPDF();
 
-require('fpdm.php');
+//set document properties
+$pdf->SetAuthor('Tom Guimberteau');
+$pdf->SetTitle('Loan Agreement');
 
-$fields = array(
-    'name'    => 'My name',
-    'address' => 'My address',
-    'city'    => 'My city',
-    'phone'   => 'My phone number'
-);
+//set font for the entire document
+$pdf->SetFont('Arial','B',20);
+$pdf->SetTextColor(50,60,100);
 
-$pdf = new FPDM('LoanAgreement.pdf');
-$pdf->Load($fields, false); // second parameter: false if field values are in ISO-8859-1, true if UTF-8
-$pdf->Merge();
-$pdf->Output();
+//set up a page
+$pdf->AddPage('P'); 
+$pdf->SetDisplayMode(real,'default');
+
+//insert an image and make it a link
+$pdf->Image('logo.png',10,20,33,0,' ','http://www.fpdf.org/');
+
+//display the title with a border around it
+$pdf->SetXY(50,20);
+$pdf->SetDrawColor(50,60,100);
+$pdf->Cell(100,10,'Loan Agreement',1,0,'C',0);
+
+//Set x and y position for the main text, reduce font size and write content
+$pdf->SetXY (10,50);
+$pdf->SetFontSize(11);
+$pdf->Write(5,'Congratulations! You have generated a PDF.');
+
+//Output the document
+$pdf->Output('Loan-Agreement.pdf','I'); 
 ?>
